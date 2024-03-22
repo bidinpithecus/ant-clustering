fd700553ccd3df06f36a88d14cfd8ab9c1950b8d
bidinpithecus <vinibidin@gmail.com>
2024-03-22 20:24:49 -0300

    main function organized so that parameters can be passed from execution

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,96 @@ use crate::ant::Ant;
 use environment::Cell;
 use environment::Grid;
 use image::{Rgb, RgbImage};
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::path::Path;
 
-const NUM_OF_ALIVE_ANTS: usize = 100;
-const NUM_OF_DEAD_ANTS: usize = 2000;
-const NUM_OF_ITERATIONS: usize = 10_000_000;
+const EMPTY_CELL_COLOR: Rgb<u8> = Rgb([200, 200, 200]);
+const DEAD_ANT_COLOR: Rgb<u8> = Rgb([0, 0, 0]);
+const ALIVE_ANT_COLOR: Rgb<u8> = Rgb([169,99,49]);
 
 mod ant;
 mod environment;
 
-fn main() {
-    let mut matrix = Grid::new(100, 100);
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
 
-    matrix.randomly_populate(NUM_OF_DEAD_ANTS, Cell::DeadAnt);
-    let mut ants_pos = matrix.randomly_populate(NUM_OF_ALIVE_ANTS, Cell::Ant);
-    let mut ants = Vec::with_capacity(NUM_OF_ALIVE_ANTS);
+    let (dir_name, num_of_rows, num_of_cols, num_of_alive_ants, num_of_dead_ants, num_of_iterations, ant_vision) = if args.len() == 8 {
+        let dir_name: String = args[1].clone();
+        let ant_vision: usize = args[2].parse()?;
+        let num_of_rows: usize = args[3].parse()?;
+        let num_of_cols: usize = args[4].parse()?;
+        let num_of_alive_ants: usize = args[5].parse()?;
+        let num_of_dead_ants: usize = args[6].parse()?;
+        let num_of_iterations: usize = args[7].parse()?;
+        (dir_name, num_of_rows, num_of_cols, num_of_alive_ants, num_of_dead_ants, num_of_iterations, ant_vision)
+    } else if args.len() == 1 {
+        let dir_name = String::from("results");
+        let ant_vision = 1;
+        let num_of_alive_ants: usize = 150;
+        let num_of_dead_ants: usize = 3_000;
+        let num_of_iterations: usize = 1_000_000;
+        let num_of_rows: usize = 100;
+        let num_of_cols: usize = 100;
+        (dir_name, num_of_rows, num_of_cols, num_of_alive_ants, num_of_dead_ants, num_of_iterations, ant_vision)
+    } else {
+        eprintln!("Usage: {} <dir_name_for_result> <ant_vision> <rows> <cols> <alive_ants> <dead_ants> <iterations>", &args[0]);
+        eprintln!("Example: {} {} {} {} {} {} {} {}", &args[0], "results", 1, 100, 100, 150, 3_000, 1_000_000);
+        std::process::exit(1);
+    };
 
-    let initial_state_image = grid_to_image(&mut matrix);
-    initial_state_image
-        .save("results/initial_state.png")
-        .unwrap();
-
-    for i in 0..NUM_OF_ALIVE_ANTS {
-        ants.push(Ant::new((ants_pos[i], Cell::Empty), 1));
+    if !Path::new(&dir_name).exists() {
+        fs::create_dir(&dir_name)?;
     }
 
-    for _ in 1..NUM_OF_ITERATIONS {
+    let mut ants: Vec<Ant> = Vec::with_capacity(num_of_alive_ants);
+
+    let mut grid = Grid::new(num_of_rows, num_of_cols);
+    grid.randomly_populate(num_of_dead_ants, Cell::DeadAnt);
+    let alive_ants_pos = grid.randomly_populate(num_of_alive_ants, Cell::Ant);
+
+    for i in 0..num_of_alive_ants {
+        ants.push(Ant::new((alive_ants_pos[i], Cell::Empty), ant_vision));
+    }
+
+    let initial_state_image = render(&mut grid);
+    initial_state_image.save(format!("{}/initial_state_for_{}x{}_grid_with_{}_alive_ants_with_radius_vision_{}_and_{}_dead_ants_and_{}_iterations.png", dir_name, num_of_rows, num_of_cols, num_of_alive_ants, ant_vision, num_of_dead_ants, num_of_iterations))?;
+
+    let final_state_image = simulate_and_render(grid, ants, num_of_iterations);
+    final_state_image.save(format!("{}/final_state_for_{}x{}_grid_with_{}_alive_ants_with_radius_vision_{}_and_{}_dead_ants_and_{}_iterations.png", dir_name, num_of_rows, num_of_cols, num_of_alive_ants, ant_vision, num_of_dead_ants, num_of_iterations))?;
+
+    Ok(())
+}
+
+fn simulate_and_render(mut grid: Grid, mut ants: Vec<Ant>, num_of_iterations: usize) -> RgbImage {
+    let mut indices_to_remove = Vec::new();
+
+    for _ in 0..num_of_iterations {
         for (_, ant) in ants.iter_mut().enumerate() {
-            ant.action(&mut matrix, true);
+            ant.action(&mut grid, true);
         }
     }
 
-    let mut indices_to_remove = Vec::new();
-
     while !ants.is_empty() {
         for (i, ant) in ants.iter_mut().enumerate() {
-            ant.action(&mut matrix, false);
+            ant.action(&mut grid, false);
             if ant.get_state() == ant::State::NotCarrying {
                 indices_to_remove.push(i);
-                matrix.set_cell(ant.get_pos(), ant.get_pos_old_cell());
+                grid.set_cell(ant.get_pos(), ant.get_pos_old_cell());
             }
         }
 
         for &index in indices_to_remove.iter().rev() {
             ants.remove(index);
-            ants_pos.remove(index);
         }
         indices_to_remove.clear();
     }
 
-    let final_state_image = grid_to_image(&mut matrix);
-    final_state_image.save("results/final_state.png").unwrap();
+    render(&mut grid)
 }
 
-fn grid_to_image(grid: &mut Grid) -> RgbImage {
+fn render(grid: &mut Grid) -> RgbImage {
     let width = grid.width() as u32;
     let height = grid.height() as u32;
     let mut img = RgbImage::new(width, height);
@@ -62,9 +99,9 @@ fn grid_to_image(grid: &mut Grid) -> RgbImage {
     for y in 0..height {
         for x in 0..width {
             let cell_color = match grid.get((x as usize, y as usize)) {
-                Cell::Empty => Rgb([200, 200, 200]),
-                Cell::Ant => Rgb([255, 0, 0]),
-                Cell::DeadAnt => Rgb([0, 0, 0]),
+                Cell::Empty => EMPTY_CELL_COLOR,
+                Cell::Ant => ALIVE_ANT_COLOR,
+                Cell::DeadAnt => DEAD_ANT_COLOR,
             };
             img.put_pixel(x, y, cell_color);
         }
